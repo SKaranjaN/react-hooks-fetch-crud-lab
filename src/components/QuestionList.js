e7c7b4abbe509dbbafa336dfcdc1647aa8546c21
Stephen <skaranja654@gmail.com>
2023-04-26 16:12:15 +0300

    POST

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,25 +1,59 @@
-import React, {useState, useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import QuestionItem from "./QuestionItem";
 
-
 function QuestionList() {
-
   const [questions, setQuestions] = useState([]);
-console.log(questions)
-  useEffect(
-    ()=>{
-      fetch("http://localhost:3000/questions")
-      .then((r)=>r.json())
-      .then((question)=>setQuestions(question))
-    },
-  []);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/questions")
+      .then((r) => r.json())
+      .then((questions) => {
+        setQuestions(questions);
+      });
+  }, []);
+
+  function handleDeleteClick(id) {
+    fetch(`http://localhost:3000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => {
+        const updatedQuestions = questions.filter((q) => q.id !== id);
+        setQuestions(updatedQuestions);
+      });
+  }
+
+  function handleAnswerChange(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((r) => r.json())
+      .then((updatedQuestion) => {
+        const updatedQuestions = questions.map((q) => {
+          if (q.id === updatedQuestion.id) return updatedQuestion;
+          return q;
+        });
+        setQuestions(updatedQuestions);
+      });
+  }
+
+  const questionItems = questions.map((quiz, index) => (
+    <QuestionItem
+      key={quiz.id}
+      question={quiz}
+      onDeleteClick={handleDeleteClick}
+      onAnswerChange={handleAnswerChange}
+    />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */
-      questions.map((quiz)=>(
-        <QuestionItem key={quiz} question={quiz}/>
-      ))}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
